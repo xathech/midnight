d699a2c158d0346b49f0b86e03d083c8da3e7c50
xathech <xathech6341@gmail.com>
2019-04-19 19:59:22 +0200

    ui wip

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -43,7 +43,7 @@
                 @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
                     <li>
                         <a class="dropdown-item" rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
-                            {{ $properties['native'] }}
+                            {{ $properties['name'] }}
                         </a>
                     </li>
                 @endforeach
