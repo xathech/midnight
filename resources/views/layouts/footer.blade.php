9371a8b1039bb02668e675b54a8cf52ee535bfd1
xathech <xathech6341@gmail.com>
2019-05-01 21:44:12 +0200

    ui-wip

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,16 @@
 <!--Footer-->
-<footer>
+<footer class="footer">
     <div class="container py-2 text-center text-light">
-        <div class="row">
+        <div class="row align-items-center">
             <div id="rights" class="col-sm">
-                <a href="https://github.com/xathech/midnight" class="btn btn-warning fa fa-github fa-2x m-1"></a>
-            <div class="small">{{ __('Midnight is being developed by Xavi Gea Ferrer')}}</div>
+                <a href="https://github.com/xathech/midnight" class="fa-fw btn btn-warning fa fa-github fa-2x m-1 p-1"></a>
+            <div class="small">{{ __('Midnight is being developed by Xavi Gea')}}</div>
+            <div class="small"><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" /></a></div>
             </div>
             <div id="social" class="col-sm">
-                <a href="https://facebook.com/" class="btn btn-warning fa fa-facebook fa-2x m-1"></a>
-                <a href="https://twitter.com/" class="btn btn-warning fa fa-twitter fa-2x m-1"></a>
-                <a href="https://reddit.com/" class="btn btn-warning fa fa-reddit fa-2x m-1"></a>
+                <a href="https://facebook.com/" class="fa-fw btn btn-warning fa fa-facebook fa-2x m-1 p-1"></a>
+                <a href="https://twitter.com/" class="fa-fw btn btn-warning fa fa-twitter fa-2x m-1 p-1"></a>
+                <a href="https://reddit.com/" class="fa-fw btn btn-warning fa fa-reddit fa-2x m-1 p-1"></a>
                 <div class="small">{{ __('Social Networks') }}</div>
             </div>
         </div>
